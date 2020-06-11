6ef204d4ed7e4f30d874c6b8d075661a3eb08875
Justin Clift <justin@postgresql.org>
2020-06-12 09:50:42 +1000

    Initial commit.  Receives UDP Mavlink packets on 14550/udp

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aler9/gomavlib"
+	"github.com/aler9/gomavlib/dialects/ardupilotmega"
+)
+
+func main() {
+	// create a node which
+	// - communicates with an UDP endpoint in server mode.
+	// - understands ardupilotmega dialect
+	// - writes messages with given system id
+	node, err := gomavlib.NewNode(gomavlib.NodeConf{
+		Endpoints: []gomavlib.EndpointConf{
+			gomavlib.EndpointUdpServer{":14550"},
+		},
+		Dialect:     ardupilotmega.Dialect,
+		OutVersion:  gomavlib.V2, // change to V1 if you're unable to write to the target
+		OutSystemId: 10,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer node.Close()
+
+	// print every message we receive
+	for evt := range node.Events() {
+		if frm, ok := evt.(*gomavlib.EventFrame); ok {
+			fmt.Printf("received: id=%d, %+v\n", frm.Message().GetId(), frm.Message())
+		}
+	}
+}
